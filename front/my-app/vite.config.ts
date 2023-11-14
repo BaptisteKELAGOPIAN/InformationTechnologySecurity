6236b0de2d7d6a363c385164e85ca2612d563826
Aymeric <aymeric.lamonerie@epitech.eu>
2023-11-14 17:13:15 +0100

    add..start securitie

--- front/my-app/vite.config.ts
+++ front/my-app/vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        https: {
+            key: '../../InformationTechnologyScurity2/privkey.pem',
+            cert: '../../InformationTechnologyScurity2/fullchain.pem',
+            minVersion: 'TLSv1',
+        },
+      },
 	plugins: [sveltekit()]
+
 });
